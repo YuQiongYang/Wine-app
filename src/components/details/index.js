6b0beb2f8a3f3e046346358009e8b6bc46a90657
YuQiong <13266465937@163.com>
2018-10-17 16:01:07 +0800

    finished goodsDetails in details

--- src/components/details/index.js
+++ src/components/details/index.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 
-import { Icon, Tabs, WhiteSpace } from 'antd-mobile'
+import { Icon, Tabs, WhiteSpace, Badge } from 'antd-mobile'
 
 import { StickyContainer, Sticky } from 'react-sticky';
 
 import Goods from './wineTabs/goods'
 import GoodsDetails from './wineTabs/goodsDetails'
 import Comment from './wineTabs/comment'
+import octicons from 'octicons'
 
 import './index.scss'
 
@@ -48,7 +49,8 @@ class Details extends Component {
                 pagesize: 20,
                 score: 0
             },
-            commentsData:[]
+            commentsData: [],
+            listAttr: []
         }
     }
     componentDidMount() {
@@ -59,7 +61,7 @@ class Details extends Component {
                     ...this.state.reqData,
                     Id
                 },
-                reqComments:{
+                reqComments: {
                     ...this.state.reqComments,
                     proid: Id
                 }
@@ -69,13 +71,14 @@ class Details extends Component {
                     if (res.status) {
                         this.setState({
                             data: res.data,
-                            banner: res.data.listpic
+                            banner: res.data.listpic,
+                            listAttr: res.data.ListAttr
                         })
                     }
                 })
-                this.$http.get('BtCApi/Item/GetComment',this.state.reqComments).then(res=>{
+                this.$http.get('BtCApi/Item/GetComment', this.state.reqComments).then(res => {
                     // console.log(res)
-                    if(res.status){
+                    if (res.status) {
                         this.setState({
                             commentsData: res.data
                         })
@@ -95,7 +98,9 @@ class Details extends Component {
         return (
             <div className="wineDetails">
                 <header>
-                    <Icon type="left"></Icon>
+                    <Icon type="left" onTouchStart={()=>{
+                        this.props.history.goBack()
+                    }}></Icon>
                     <p>
                         {data.ProductName}
                     </p>
@@ -105,14 +110,37 @@ class Details extends Component {
                     <StickyContainer>
                         <Tabs tabs={tabs}
                             initalPage={'t2'}
+                            useOnPan={false}
                             renderTabBar={renderTabBar}
                         >
                             <Goods data={data} banner={this.state.banner} commentsData={this.state.commentsData} />
-                            <GoodsDetails />
-                            <Comment commentsData={this.state.commentsData}/>
+                            <GoodsDetails banner={this.state.banner} listAttr={this.state.listAttr} />
+                            <Comment commentsData={this.state.commentsData} />
                         </Tabs>
                     </StickyContainer>
                     <WhiteSpace />
+                </div>
+
+                <div className="detailsFoot">
+                    <a href="javascript:" style={{ width: '15%', height: '100%' }} onTouchStart={()=>{
+                        this.props.history.push('/')
+                    }}>
+                        <span dangerouslySetInnerHTML={{ __html: octicons.home.toSVG() }} />
+                        <span>首页</span>
+                    </a>
+                    <a href="javascript:" style={{ width: '15%', height: '100%' }} onTouchStart={()=>{
+                            this.props.history.push('/cart')
+                        }}>
+                        <span>
+                            <span dangerouslySetInnerHTML={{ __html: octicons.inbox.toSVG() }} />
+                            <i>
+                            <Badge text={1} />
+                            </i>
+                        </span>
+                        <span>购物车</span>
+                    </a>
+                    <a href="javascript:" style={{ width: '35%', height: '100%' }}>加入购物车</a>
+                    <a href="javascript:" style={{ width: '35%', height: '100%' }}>立即购买</a>
                 </div>
             </div>
         )
--- src/components/details/wineTabs/goodsDetails.js
+++ src/components/details/wineTabs/goodsDetails.js
@@ -1,9 +1,70 @@
 import React, { Component } from 'react';
 
-class GoodsDetails extends Component{
-    render(){
-        return(
-            <div>GoodsDetails</div>
+import { Tabs, WhiteSpace } from 'antd-mobile';
+
+class GoodsDetails extends Component {
+    constructor() {
+        super()
+        this.state = {
+            tabs: [{
+                id: 1,
+                title: '商品介绍'
+            }, {
+                id: 2,
+                title: '规格参数'
+            }, {
+                id: 3,
+                title: '包装售后'
+            }],
+            idx: 1
+        }
+    }
+    render() {
+        let { tabs, idx } = this.state
+        let { banner, listAttr } = this.props
+        console.log(listAttr)
+        return (
+            <div className="goodsDetails">
+                <WhiteSpace />
+                <Tabs tabs={tabs} initialPage={0} animated={false} useOnPan={false}>
+                    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', backgroundColor: '#fff' }}>
+                        <ul className="goodsImg">
+                            {
+                                banner.map(item => {
+                                    return (
+                                        <li key={item.Pic}>
+                                            <img src={`http://img0.gjw.com/product/${item.Pic}`} />
+                                        </li>
+                                    )
+                                })
+                            }
+                        </ul>
+                    </div>
+                    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', backgroundColor: '#fff' }}>
+                        <table border="1">
+                            <tbody>
+                                {
+                                    listAttr.map(item => {
+                                        return <tr key={item.AttrTitle}>
+                                            <td>
+                                                {item.AttrTitle}
+                                            </td>
+                                            <td>
+                                                {item.AttrVal}
+                                            </td>
+                                        </tr>
+                                    })
+                                }
+                            </tbody>
+                        </table>
+                    </div>
+                    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', backgroundColor: '#fff' }}>
+                        Content of third tab
+                    </div>
+                </Tabs>
+                <WhiteSpace />
+
+            </div>
         )
     }
 }
